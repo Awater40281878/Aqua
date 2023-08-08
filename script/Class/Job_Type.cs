531a89d8315be5080c21bf0d03b20c4a9df51054
Awater40281878 <128442184+Awater40281878@users.noreply.github.com>
2023-08-08 22:32:36 +0800

    Create Job_Type.cs

---
+++ script/Class/Job_Type.cs
@@ -0,0 +1,25 @@
+using System;
+[Serializable]
+public enum Job_Type
+{	
+	//無
+	Null,
+	//劍士
+	Saber,
+	//弓箭手
+	Archer,
+	//坦克
+	Tank,
+	//女巫
+	Witch,
+	//無賴
+	Scoundrel,
+	//狂戰士
+	Berserker,
+	//火焰術師
+	FireMagic,
+	//神箭手
+	Sagittarius,
+	//重生點
+	SpawnPoint,
+}
