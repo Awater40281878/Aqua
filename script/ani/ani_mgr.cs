773def193a834b0d2303efaef04ac990ed20c0da
Awater40281878 <128442184+Awater40281878@users.noreply.github.com>
2023-08-08 22:32:17 +0800

    Create ani_mgr.cs

---
+++ script/ani/ani_mgr.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ani_mgr : MonoBehaviour
+{
+    public static ani_mgr instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    [System.Serializable]
+    public class FoldableProperties
+    {
+        public List<spirt_Date> ain;
+    }
+
+    [System.Serializable]
+    public class AnimationGroup
+    {
+        [SerializeField]
+        private string title = "Animation Group";
+        public List<spirt_Date> Animation = new List<spirt_Date>();
+
+        public string Title => title;
+    }
+
+    [SerializeField]
+    public List<AnimationGroup> soldierAnimation = new List<AnimationGroup>();
+}
