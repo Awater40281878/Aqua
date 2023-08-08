7f18f2329f4222ba7cce040180202aaac0f0c1a8
Awater40281878 <128442184+Awater40281878@users.noreply.github.com>
2023-08-08 22:33:22 +0800

    Create FrameRateController.cs

---
+++ script/FrameRateController.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading;
+using UnityEngine;
+
+public class FrameRateController : MonoBehaviour
+{
+    private const int TargetFrameRate = 100; // 目标帧率
+
+    void Start()
+    {
+        Application.targetFrameRate = TargetFrameRate;
+    }
+
+    void Update()
+    {
+        // 检查当前帧率
+        if (1f / Time.deltaTime > TargetFrameRate)
+        {
+            // 计算需要等待的时间（以秒为单位）
+            float waitTime = 1f / TargetFrameRate - Time.deltaTime;
+
+            // 等待一段时间
+            if (waitTime > 0)
+            {
+                Thread.Sleep((int)(waitTime * 1000));
+            }
+        }
+    }
+}
