c36b4e698fc20041cb254b0c3ddba5a9899aeebc
Awater40281878 <128442184+Awater40281878@users.noreply.github.com>
2023-09-08 05:47:58 +0800

    Create behaviorMod.cs

---
+++ script/Class/behaviorMod.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public enum behaviorMod
+{
+    NotActed,
+    Acted,
+    Idle,
+    OnAttack,
+    OnMove,
+    OnMoveing_first,
+    OnMoveing_second,
+    OnHealing,
+    OnDiscussion,
+    OnUesitem,
+    choiceAttack_mode,
+
+
+}
