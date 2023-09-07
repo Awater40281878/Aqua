0c87dd894485b33070bc760d56290d5c348b38d7
Awater40281878 <128442184+Awater40281878@users.noreply.github.com>
2023-09-08 05:48:00 +0800

    Update Unit_map_Date.cs

--- script/Class/Unit_map_Date.cs
+++ script/Class/Unit_map_Date.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 [Serializable]
 public class Unit_map_Date
 {
-	public Unit_Loc loc;
+	public Vector3 loc;
 	public Unit_Data date;
+	public bool IsUesItem = false;
+	public bool IsAttack = false;
+	public int CanMoveCont = 1;
+	public int MoveCont = 0;
+	public int MoveStep = 0;
+	public bool EnemyInRange = false;
+	public bool Idle = false;
 
-	public Unit_map_Date(Unit_Loc _loc, Unit_Data _date)
+
+
+	public Unit_map_Date(Vector3 _loc, Unit_Data _date)
 	{
 		this.loc = _loc;
 		this.date = _date;
