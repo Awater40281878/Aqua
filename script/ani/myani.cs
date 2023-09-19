9acb018b0c41bfaaae3d9b7eb7ecfbb9605d93ca
Awater40281878 <128442184+Awater40281878@users.noreply.github.com>
2023-09-19 16:49:07 +0800

    Update myani.cs

--- script/ani/myani.cs
+++ script/ani/myani.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class myani : MonoBehaviour
 {
-    public GameObject si;
     public static myani instance;
 	private void Awake()
 	{
@@ -15,39 +15,64 @@ public class myani : MonoBehaviour
 
 		}
 	}
-    public void Start_Battle()
+	public void Start_Battle()
+	{	
+		GameObject animeUI = UIMgr.instance.GetChild(GameObject.Find("UIanime"), 1);
+		animeUI.SetActive(true);
+		StartCoroutine(Start_Battle_ani(4f));
+    }
+	//開始戰鬥動畫
+	IEnumerator Start_Battle_ani(float costTime)
 	{
-        
-        StartCoroutine(Start_Battle_ani(1f));
-    }
-    //開始戰鬥動畫
-    IEnumerator Start_Battle_ani(float costTime)
-    {
-        
-        si.SetActive(true);
-        Image filledImage = GameObject.Find("battleStartIcon").GetComponent<Image>();
-        
-        float elapsedTime = 0.0f;
-        float startFillAmount = 1.0f;
-        float targetFillAmount = 0.0f;
+		Image img = GameObject.Find("UIanime").transform.GetChild(0).GetComponent<Image>();
 
-        while (elapsedTime < costTime)
-        {
-            // 使用插值函式計算當前填充量
-            float currentFillAmount = Mathf.Lerp(startFillAmount, targetFillAmount, elapsedTime / costTime);
+		float targetAlpha = 1.0f; // 最終透明度
+		float halfTime = costTime / 4.0f; // 從0到255的一半時間
 
-            // 將填充量設置到 Image 組件上
-            filledImage.fillAmount = currentFillAmount;
+		// 從0到255的透明度增加速度
+		float deltaAlpha = targetAlpha / halfTime;
 
-            // 增加經過的時間
-            elapsedTime += Time.deltaTime;
+		// 從0到255的漸顯
+		while (img.color.a < targetAlpha)
+		{
+			Color newColor = img.color;
+			newColor.a += deltaAlpha * Time.deltaTime;
+			img.color = newColor;
+			yield return null;
+		}
 
-            // 等待下一個幀
-            yield return null;
-        }
-        si.SetActive(false);
-        // 確保填充量在結束時為目標值
-        filledImage.fillAmount = targetFillAmount;
-    }
+		// 等待一段時間，讓透明度保持在255
+		yield return new WaitForSeconds(halfTime);
 
+		// 從255到0的透明度減小速度
+		deltaAlpha = targetAlpha / halfTime;
+
+		// 從255到0的漸隱
+		while (img.color.a > 0)
+		{
+			Color newColor = img.color;
+			newColor.a -= deltaAlpha * Time.deltaTime;
+			img.color = newColor;
+			yield return null;
+		}
+		switch (battleRound.instance.Phase)
+		{
+			case 1:
+
+				break;
+			case 2:
+				battleRound.instance.StartEY();
+				break;
+			default:
+				break;
+		}
+		
+		// 確保透明度最終為0
+		Color finalColor = img.color;
+		finalColor.a = 0;
+		img.color = finalColor;
+		img.gameObject.SetActive(false);
+		// 在此之後你可以執行其他操作，或者在淡入淡出完成後繼續邏輯
+	}
+	
 }
